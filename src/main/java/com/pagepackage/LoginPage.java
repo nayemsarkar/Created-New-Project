17da926f0c6eed3ca934a96efaba4bdc39491aa2
Naimul Haque <esjgroupinc8@gmail.com>
2022-07-24 04:01:54 +0600

    new page code added

--- src/main/java/com/pagepackage/LoginPage.java
+++ src/main/java/com/pagepackage/LoginPage.java
@@ -1,9 +1,42 @@
 package com.pagepackage;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class LoginPage {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		WebDriver driver = new ChromeDriver();	
+		
+		System.out.println(driver.getTitle());
+		
+		
+		// All test is passed----below-----Automation login 
+		
+				driver.get("http://qa.user.student.ui.eschooljourney.com:3001/");
+				
+				
+				//driver.findElement(By.linkText("Forgot Password?")).click();
+				
+				//Thread.sleep(2000);
+				
+				driver.findElement(By.id("username_input")).click();
+				
+				//Thread.sleep(2000);
+				driver.findElement(By.id("username_input")). sendKeys("studentUser");
+				
+				//Thread.sleep(3000);
+				
+				//Thread.sleep(2000);
+				driver.findElement(By.id("password_input")).click();
+				
+				//Thread.sleep(2000);
+				driver.findElement(By.id("password_input")). sendKeys("1234");
+				
+
 
 	}
 
